b9d21955e19ed4ed7fcd15e9d1ec1bfe181a0716
Madhan  S <128589634+Madhan-2213@users.noreply.github.com>
2024-08-20 16:10:33 +0530

    Concatenate.java

---
+++ Concatenate.java
@@ -0,0 +1,8 @@
+public class Concatenate{
+    public static void main(String[] args){
+        String fname,lname;
+        fname  = "Madhan";
+        lname = "Lucifer";
+        System.out.println(fname + " "+lname);
+}
+}
