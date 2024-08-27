3466bdba7da5c5aa462effacbd904f2326913e09
Madhan  S <128589634+Madhan-2213@users.noreply.github.com>
2024-08-27 10:37:28 +0530

    greatestno.java

---
+++ greatestno.java
@@ -0,0 +1,6 @@
+class Main{
+    public static void main(String[] args){
+        int a = 10,b = 40,c = 30;
+        System.out.println((a>b)?((a>c)?"a is max":"c is max"):((b>c)?"b is max":"c is max"));
+    }
+}
