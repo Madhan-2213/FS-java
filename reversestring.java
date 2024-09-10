49a4e74de551acc8727b7490faebf92bf2af4a4c
Madhan  S <128589634+Madhan-2213@users.noreply.github.com>
2024-09-10 12:23:24 +0530

    reversestring.java

---
+++ reversestring.java
@@ -0,0 +1,8 @@
+public class main {
+	
+	  public static void main(String[] args) {
+	   String santhosh = "monitor mouse keyboard";
+	   String arr[] = santhosh.split(" ");
+	   for(int i = arr.length-1;i>=0;i--) {
+		   System.out.print(arr[i]+" ");
+	}}}
