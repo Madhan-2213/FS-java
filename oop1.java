32751ad34e2c9746cba1b316ec3c101079dc3fee
Madhan  S <128589634+Madhan-2213@users.noreply.github.com>
2024-09-11 11:03:33 +0530

    oop1.java

---
+++ oop1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class madhan {
+	
+		int id;
+		String name; 
+	}
+	public class main{
+	 public static void main(String[] args) {
+		  madhan m = new madhan();
+		  m.id = 12;
+		  m.name = "Madhan";
+		  System.out.println(m.id);
+		  System.out.println(m.name);
+		 
+	 }
+	}
+	
